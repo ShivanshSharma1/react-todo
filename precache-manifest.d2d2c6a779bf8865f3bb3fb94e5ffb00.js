bd59a61c88b4f2e57ffac088fdbe41f8c53330c7
ShivanshSharma1 <shivanshs331@gmail.com>
2020-09-01 20:07:03 +0530

    Updates

---
+++ precache-manifest.d2d2c6a779bf8865f3bb3fb94e5ffb00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eccd8df01d26297d093a7a44f39b8a1",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "1217e0b60075dd7d61bf",
+    "url": "/react-todo/static/js/2.6f79aa35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.6f79aa35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d193204753ab0fb05606",
+    "url": "/react-todo/static/js/main.47540660.chunk.js"
+  },
+  {
+    "revision": "9f8a44e498a833cbd850",
+    "url": "/react-todo/static/js/runtime-main.9e350677.js"
+  }
+]);
